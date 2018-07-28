b4115e570a7d32c7ff7a2ed36699f5dbdfb7c0de
Yago Mera <yagomera@gmail.com>
2018-07-28 21:31:40 +0200

    variable entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -15,7 +15,6 @@ if (process.env.NODE_ENV === 'dev')
     urlDB = 'mongodb://localhost:27017/cafe';
 else
 
-    urlDB = 'mongodb://yagomera:fkm03fkdlq@ds257851.mlab.com:57851/cafe';
-
+    urlDB = process.env.MONGO_URI.
 
 process.env.URLDB = urlDB;
